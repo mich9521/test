23865a2ce99cc594c06d9682fd1c5f8f24f2de9d
Michelle <jmvs012@hotmail.com>
2020-07-09 10:02:55 -0600

    commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frontend Developer's books</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Ecommerce</h1>
+
+        <form action="">
+            <div class="row">
+                <div class="input-group">
+                    <div class="input-group-text">$</div>
+                    <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+<script src="https://kit.fontawesome.com/0f7c40475c.js" crossorigin="anonymous"></script>
+</html>
